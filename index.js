ebb6b256ab002a777a619f78acadc814aec3fe9c
mrmirror5 <mirstumiro@gmail.com>
2026-07-08 19:21:30 +0300

    Wanna work?

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 app.use(cors())
 
 app.use(express.static('dist'))
+
 let notes = [
   {
     id: '1',
@@ -34,9 +35,9 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
