4728e5f3bdd4426acdcd11faf075b097b1a50167
mrmirror5 <mirstumiro@gmail.com>
2026-07-08 18:39:04 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
+
 let notes = [
   {
     id: '1',
@@ -88,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
